83868d53896613ddf4dcfe80d0bb9ef6b7f37d00
Bifern35 <s5930213005@phuket.psu.ac.th>
2018-12-15 14:43:18 +0700

    add path /Employees

--- final/src/router.js
+++ final/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import employees from './views/Employees.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/Employees',
+      name: 'Employees',
+      component: Employees
     },
     {
       path: '/about',
