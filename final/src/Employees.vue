30b52e9c4e99d0f92e75f8eb150c05f4383f4002
Bifern35 <s5930213005@phuket.psu.ac.th>
2018-12-15 14:55:53 +0700

    add template

---
+++ final/src/Employees.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>Employees List</h1>
+        <b-table striped hover :items="employees" :fields="fields" :per-page="pageSize" :current-page="pageIndex"></b-table>
+        <b-pagination size="md" :total-rows="products.length" v-model="pageIndex" :per-page="pageSize"></b-pagination>
+    </div>
+</template>
